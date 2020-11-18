84069287a3f7572efa3665c17dab64f673358dc4
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2020-11-17 19:49:05 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 5432,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/notefuldb',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/notefuldb',
 }
